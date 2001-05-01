f4714e3e1bf0a42a063733193fb2472cbae1e443
alexios <alexios@bedroomlan.org>
2001-05-01 22:25:53 +0000

    Updated to include patchlevel versioning.

--- src/lsm/mklsm.sh
+++ src/lsm/mklsm.sh
@@ -2,11 +2,13 @@
 
 major=$1
 minor=$2
+patch=$3
 date=`date +'%d%b%y'|tr '[:lower:]' '[:upper:]'`
 
-gawk <lsm/$3.lsm -vmajor=$major -vminor=$minor -vdate=$date -- '{
+gawk <lsm/$3.lsm -vmajor=$major -vminor=$minor -vpatch=$patch -vdate=$date -- '{
     gsub(/@major/,major);
     gsub(/@minor/,minor);
+    gsub(/@patch/,patch);
     gsub(/@date/,date);
     print;
 }'
