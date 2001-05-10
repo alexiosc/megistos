d98b13dba560dd1de4d4e056ee1272304f79a866
alexios <alexios@bedroomlan.org>
2001-05-10 10:18:39 +0000

    Started making things compliant with 0INSTALL-NOTES, install scripts and packagers.

--- lib/src/modules/telecon/plugins/bjack/bjver.h
+++ lib/src/modules/telecon/plugins/bjack/bjver.h
@@ -3,5 +3,5 @@
 #define BJ_MAJORVERSION "0"
 #define BJ_MINORVERSION "7"
 #define BJ_COMPILED_BY  "bbs@vennea"
-#define BJ_MAKE_NUMBER  "300"
-#define BJ_DATE         "01-05-01 23:22"
+#define BJ_MAKE_NUMBER  "302"
+#define BJ_DATE         "01-05-01 23:46"
