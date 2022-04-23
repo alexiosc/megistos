cbff8937a91010d1cee563f30eb518824a4a022e
Alexios Chouchoulas <alexios@bedroomlan.org>
2022-04-23 21:03:27 +0300

    Fix pytest reporting, add coverage reports

--- python/pytest.sh
+++ python/pytest.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 BASEDIR=$(realpath $PWD)
+BASEDIR=.
 export PYTHONPATH=$BASEDIR:$BASEDIR/lib
 
-OPTS="--doctest-modules --ff --strict-markers -vvv --rootdir=$PWD"
+OPTS="--doctest-modules --ff --strict-markers -vvv "
+#OPTS+="--rootdir=$PWD "
+OPTS+="--cov-report=html --cov=megistos "
 
 if [[ "$*" =~ \.py ]]; then
     pytest $OPTS "$@"
